b9adfe882aa4ad74a0a2eaaf19f41ef2d790c064
Anthony <anthonypoullain.pro@gmail.com>
2023-06-28 22:57:45 +0200

    :zap: Disable lazy loading main image and screenshots

--- src/components/ScreenshotSection/ScreenshotSection.tsx
+++ src/components/ScreenshotSection/ScreenshotSection.tsx
@@ -4,32 +4,33 @@ import { SectionWithTitle } from '..';
 import Image from 'next/image';
 
 type Screenshot = {
-  id: number;
-  image: string;
+	id: number;
+	image: string;
 };
 
 function ScreenshotSection({ screenshots }: { screenshots: Screenshot[] }) {
-  return (
-    <SectionWithTitle title="Screenshots" border={true}>
-      <div className={`sm:grid-cols-${screenshots?.length} grid gap-2`}>
-        {screenshots.map((screen) => (
-          <div
-            className="h-fit max-h-32 w-full overflow-hidden rounded-md"
-            key={screen.id}
-          >
-            <Image
-              className="transition-opacity duration-1000 opacity-0"
-              src={screen.image}
-              alt="screenshot"
-              width={600}
-              height={80}
-              onLoadingComplete={(image) => image.classList.remove('opacity-0')}
-            />
-          </div>
-        ))}
-      </div>
-    </SectionWithTitle>
-  );
+	return (
+		<SectionWithTitle title="Screenshots" border={true}>
+			<div className={`sm:grid-cols-${screenshots?.length} grid gap-2`}>
+				{screenshots.map((screen) => (
+					<div
+						className="h-fit max-h-32 w-full overflow-hidden rounded-md"
+						key={screen.id}
+					>
+						<Image
+							className="transition-opacity duration-1000 opacity-0"
+							src={screen.image}
+							alt="screenshot"
+							width={600}
+							height={80}
+							priority={true}
+							onLoadingComplete={(image) => image.classList.remove('opacity-0')}
+						/>
+					</div>
+				))}
+			</div>
+		</SectionWithTitle>
+	);
 }
 
 export default ScreenshotSection;
