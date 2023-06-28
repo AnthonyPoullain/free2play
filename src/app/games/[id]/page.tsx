2044961b0e557079926b57e465fe38aa6221d027
Anthony <anthonypoullain.pro@gmail.com>
2023-06-28 22:31:43 +0200

    :children_crossing: Screenshot section -> client (better loading)

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import { Games } from '@/src/services';
-import { CardGrid, SectionWithTitle } from '@/src/components';
+import {
+  CardGrid,
+  ScreenshotSection,
+  SectionWithTitle,
+} from '@/src/components';
 import { BsFillPlayFill, BsTwitch, BsYoutube } from 'react-icons/bs';
 import Link from 'next/link';
 import { shuffleArray } from '@/src/utils';
@@ -71,25 +75,7 @@ export default async function Game({ params }: { params: { id: string } }) {
         <section className="flex flex-col gap-4"></section>
       </div>
       {game.screenshots && game.screenshots.length ? (
-        <SectionWithTitle title="Screenshots" border={true}>
-          <div
-            className={`sm:grid-cols-${game?.screenshots?.length} grid gap-2`}
-          >
-            {game?.screenshots?.map((screen) => (
-              <div
-                className="h-fit max-h-32 w-full overflow-hidden rounded-md"
-                key={screen.id}
-              >
-                <Image
-                  src={screen.image}
-                  alt="screenshot"
-                  width={600}
-                  height={80}
-                />
-              </div>
-            ))}
-          </div>
-        </SectionWithTitle>
+        <ScreenshotSection screenshots={game?.screenshots} />
       ) : null}
 
       <SectionWithTitle
@@ -104,56 +90,60 @@ export default async function Game({ params }: { params: { id: string } }) {
 
       <SectionWithTitle title={'More info'}>
         <table className="align-left sm:mx-0 border-zinc-300 w-full max-w-md p-2 mx-auto mt-2 text-left border border-separate rounded">
-          <tr>
-            <td>Title</td>
-            <td>{game?.title}</td>
-          </tr>
-          <tr>
-            <td>Genre</td>
-            <td>{game?.genre}</td>
-          </tr>
-          <tr>
-            <td>Platform</td>
-            <td>{game?.platform}</td>
-          </tr>
-          <tr>
-            <td>Publisher</td>
-            <td>{game?.publisher}</td>
-          </tr>
-          <tr>
-            <td>Developer</td>
-            <td>{game?.developer}</td>
-          </tr>
-          <tr>
-            <td>Release Date</td>
-            <td>{new Date(game.release_date).toLocaleDateString()}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Title</td>
+              <td>{game?.title}</td>
+            </tr>
+            <tr>
+              <td>Genre</td>
+              <td>{game?.genre}</td>
+            </tr>
+            <tr>
+              <td>Platform</td>
+              <td>{game?.platform}</td>
+            </tr>
+            <tr>
+              <td>Publisher</td>
+              <td>{game?.publisher}</td>
+            </tr>
+            <tr>
+              <td>Developer</td>
+              <td>{game?.developer}</td>
+            </tr>
+            <tr>
+              <td>Release Date</td>
+              <td>{new Date(game.release_date).toLocaleDateString()}</td>
+            </tr>
+          </tbody>
         </table>
       </SectionWithTitle>
 
       <SectionWithTitle title="System requirements">
         <table className="sm:text-left w-full p-2 text-center rounded">
-          {game.minimum_system_requirements &&
-          !game.platform.toLowerCase().includes('browser') ? (
-            Object.values(
-              game?.minimum_system_requirements as SystemRequirements
-            ).map((requirement: string) => (
-              <tr key={crypto.randomUUID()}>
-                <td>{requirement}</td>
+          <tbody>
+            {game.minimum_system_requirements &&
+            !game.platform.toLowerCase().includes('browser') ? (
+              Object.values(
+                game?.minimum_system_requirements as SystemRequirements
+              ).map((requirement: string) => (
+                <tr key={crypto.randomUUID()}>
+                  <td>{requirement}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td className="font-normal">
+                  <strong className="font-bold">{game?.title}</strong> is a
+                  browser based game and should run smoothly on practically any
+                  PC with an updated web browser. If you have old hardware or
+                  software, you may still be able to play, but your game
+                  experience may suffer. For the best gameplay experience, we
+                  recommend the latest versions of Chrome or Firefox.
+                </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td className="font-normal">
-                <strong className="font-bold">{game?.title}</strong> is a
-                browser based game and should run smoothly on practically any PC
-                with an updated web browser. If you have old hardware or
-                software, you may still be able to play, but your game
-                experience may suffer. For the best gameplay experience, we
-                recommend the latest versions of Chrome or Firefox.
-              </td>
-            </tr>
-          )}
+            )}
+          </tbody>
         </table>
       </SectionWithTitle>
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,27 +3,29 @@ import CardGrid from './CardGrid/CardGrid';
 import Header from './Header/Header';
 import Label from './Label/Label';
 import Logo from './Logo/Logo';
+import ScreenshotSection from './ScreenshotSection/ScreenshotSection';
 import SearchBar from './SearchBar/SearchBar';
 import SearchModal from './SearchModal/SearchModal';
 import SectionWithTitle from './SectionWithTitle/SectionWithTitle';
 import Sidebar from './Sidebar/Sidebar';
 import {
-	SkeletonImage,
-	SkeletonParagraph,
-	SkeletonTitle,
+  SkeletonImage,
+  SkeletonParagraph,
+  SkeletonTitle,
 } from './Skeletons/Skeletons';
 
 export {
-	Header,
-	Logo,
-	Card,
-	SearchModal,
-	SearchBar,
-	Label,
-	Sidebar,
-	CardGrid,
-	SectionWithTitle,
-	SkeletonTitle,
-	SkeletonParagraph,
-	SkeletonImage,
+  Header,
+  Logo,
+  Card,
+  SearchModal,
+  SearchBar,
+  Label,
+  Sidebar,
+  CardGrid,
+  SectionWithTitle,
+  SkeletonTitle,
+  SkeletonParagraph,
+  SkeletonImage,
+  ScreenshotSection,
 };
